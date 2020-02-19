a82266c56868d1c7d987004d6aa8b1246bbac446
Anthony Truong <anthonyytruong@gmail.com>
2020-02-19 20:21:16 +0100

    [UPDATE] Formatter - Changed name, and handling single formatting

--- src/Renamer/Formatter.py
+++ src/Renamer/Formatter.py
@@ -8,10 +8,15 @@ def filename_parsing(files):
     return parsed_files
 
 
-def format_filename(parsed_files):
-    output_filenames = []
+def format_filename(file):
+    file_dict = guessit(file)
+    episode = str(file_dict['episode']) if file_dict['episode'] >= 10 else "0" + str(file_dict['episode'])
+    return ("%s - %s [%s].%s" % (file_dict['title'], episode,
+                                 file_dict['screen_size'], file_dict['container']))
+
+
+def get_all_formatted_filenames(parsed_files):
+    format_files = []
     for file_dict in parsed_files:
-        output_filenames.append("%s - %d [%s].%s"
-                                % (file_dict['title'], file_dict['episode'],
-                                   file_dict['screen_size'], file_dict['container']))
-    return output_filenames
+        format_files.append(format_filename(file_dict))
+    return format_files
